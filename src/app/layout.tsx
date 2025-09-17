56f15155df024320696dc6f997a4daa1416aeee4
Mentor <37953260+senthilkumar979@users.noreply.github.com>
2025-09-17 23:57:22 +0200

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Lexend } from "next/font/google";
 import "./globals.css";
@@ -63,6 +64,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
